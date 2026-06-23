dfb48ac772f8579ae1902be5e87a8c3fa240b56c
adityadubey990 <adityadubeyy19@gmail.com>
2026-06-23 22:50:39 +0530

    template commit

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT = '-e .'
+
 def get_requirements(file_path:str)->List[str]:
     '''
     This function will return the list of requirements
@@ -10,9 +12,11 @@ def get_requirements(file_path:str)->List[str]:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
 
-    
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements 
 
-    return requirements
+
 
 
 
