53e4e63192b04031a16cc39ba698f30ea71b1d66
adityadubey990 <adityadubeyy19@gmail.com>
2026-07-07 18:35:19 +0530

    ingestion and mysql

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from src.mlproject1.logger import logging
 from src.mlproject1.exception import CustomException
+from src.mlproject1.components.data_ingestion import DataIngestion
+from src.mlproject1.components.data_ingestion import DataIngestionConfig
+
 import sys
 
 
 if __name__=="__main__":
     logging.info("The execution has started")
-    
+
     try:
-        a=1/0
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
+    
+        
+
+    
+        
     except Exception as e:
-        logging.info("Divide by zero error has occured")
+        logging.info("Custom Exception")
         raise CustomException(e,sys)
